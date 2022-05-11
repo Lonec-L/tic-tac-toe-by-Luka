c4390cf63a2b3005276d76e23dbeb5e95ff3aeb7
Luka Lonec <luka.lonec@gmail.com>
2022-05-11 21:44:18 +0200

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-    script: "index.js",
-    // Specify which folder to watch
-    watch: true,
-    // Specify delay between watch interval
-    watch_delay: 1000,
-    // Specify which folder to ignore 
-    ignore_watch : ["node_modules", ".git"],
-  }
-  
+  apps: [{
+    name: "tic-tac-toe",
+    script: "index.js"
+  }]
+}
