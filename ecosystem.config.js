87dfd12a69a9d306398bda702a5e3b0df80f4e06
Luka Lonec <luka.lonec@gmail.com>
2022-05-11 21:15:00 +0200

    config for pm2 auto restart

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    script: "index.js",
+    // Specify which folder to watch
+    watch: true,
+    // Specify delay between watch interval
+    watch_delay: 1000,
+    // Specify which folder to ignore 
+    ignore_watch : ["node_modules", ".git"],
+  }
+  
