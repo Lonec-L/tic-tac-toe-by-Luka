d0055fbfc47ec133fff6101c47637f6ff2051814
Luka Lonec <luka.lonec@gmail.com>
2022-06-23 17:27:37 +0200

    fixed gitignore and added some scripts

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/flutter_client/build/web/'));
 
 const { Game } = require('./game');
 
@@ -171,9 +171,9 @@ io.on('connection', (socket) => {
     })
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/flutter_client/build/web/index.html');
+// });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
